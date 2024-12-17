524035ed94717c56eb828a618d9550bb4b9bf30b
KumarS14 <sonjaykumar1404@gmail.com>
2024-12-17 14:37:50 +0000

    added nav bar interactivity so when buttons are pressed they redirect to the appropiate page

--- script.js
+++ script.js
@@ -10,4 +10,13 @@ $(document).ready(function() {
     $('#view-all-card').on('click', function() {
         window.location.href = 'view-all-page.html';
     });
+    $('#Home-Button').on('click', function() {
+        window.location.href = 'home-page.html';
+    });
+    $('#Search-Button').on('click', function() {
+        window.location.href = 'search-page.html';
+    });
+    $('#View-All-Button').on('click', function() {
+        window.location.href = 'view-all-page.html';
+    });
 });
