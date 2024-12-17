3073a45ccac5322ac15539708c425501ce313e70
KumarS14 <sonjaykumar1404@gmail.com>
2024-12-17 12:53:48 +0000

    added js code for page redirection for the cards

---
+++ script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    $('#about-card').on('click', function() {
+        window.location.href = 'about-page.html'; 
+    });
+
+    $('#search-card').on('click', function() {
+        window.location.href = 'search-page.html';
+    });
+
+    $('#view-all-card').on('click', function() {
+        window.location.href = 'view-all-page.html';
+    });
+});
