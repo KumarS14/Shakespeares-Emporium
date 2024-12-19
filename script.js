20f72fdf1f2a6af73db41287fb3d31bea2285203
KumarS14 <sonjaykumar1404@gmail.com>
2024-12-19 14:02:57 +0000

    created function that sends GET request to api in order to recieve data about shakespeare

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
+$(document).ready(function() { // Functionality for the homepage where depending on what card they clicked a new widow is show
     $('#about-card').on('click', function() {
         window.location.href = 'about-page.html'; 
-    });
+    });// redirects the user to a new page by updating the browsers current location.
 
     $('#search-card').on('click', function() {
         window.location.href = 'search-page.html';
@@ -19,16 +19,38 @@ $(document).ready(function() {
     $('#View-All-Button').on('click', function() {
         window.location.href = 'view-all-page.html';
     });
-    $('#video-container').on('click', function(){
-        var iframe = $('<iframe>', {
-            src: 'https://www.youtube.com/embed/geev441vbMI?autoplay=1',
-            class: 'video-iframe',
-            allow: 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture',
-            allowfullscreen: true
-        });
-        $(this).replaceWith(iframe);
-    });
 });
+    // When the video container is clicked the video container is replaced with an embedded YouTube iframe to watch the video
+$('#video-container').on('click', function() {
+    // Create an iframe element with the necessary attributes for YouTube playback
+    var iframe = $('<iframe>', {
+        src: 'https://www.youtube.com/embed/geev441vbMI?autoplay=1', // URL for the video, autoplay starts playback immediately
+        class: 'video-iframe', // Apply a class for styling purposes
+        allow: 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture', // Permissions for video playback
+        allowfullscreen: true // Allow full-screen mode
+    });
+
+    // video container is replaced with the newly created iframe
+    $(this).replaceWith(iframe);
+});
+
+// empty array to store the filtered list of works
+let filteredWorks = [];
+
+// function serves tofFetch Shakespeare's works from the Gutendex API
+const fetchShakespeareWorks = () => {
+    // I used jQuery's AJAX method to make a GET request to the API as its alot easier and simpler than traditional JS
+    $.ajax({
+        url: 'https://gutendex.com/books?search=shakespeare', // API endpoint to search for works by Shakespeare
+        method: 'GET', // HTTP method to fetch data
+        success: function(data) { // Callback function for a successful API response
+            filteredWorks = data.results; // Store the list of works in the `filteredWorks` array
+        },
+        error: function(error) { // Callback function to handle errors during the API request
+            console.error('Error fetching data:', error); // Log the error to the console for debugging
+        }
+    });
+};
 
 
 
